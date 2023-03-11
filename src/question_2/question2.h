dab311c743c71f83760f8484c33bda8e8b73d63a
Otter2022 <74218306+Otter2022@users.noreply.github.com>
2023-03-11 06:27:08 +0000

    Question 2 and 3

--- src/question_2/question2.h
+++ src/question_2/question2.h
@@ -1,1 +1,9 @@
+#include<string>
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::string;
+
 bool test_config();
+string decimal_to_hex(int decimal);
--- src/question_3/question3.h
+++ src/question_3/question3.h
@@ -1,1 +1,10 @@
 bool test_config();
+#include<vector>
+#include<iostream>
+
+using std::vector;
+using std::cout;
+using std::cin;
+
+bool is_prime(int num);
+vector<int> get_primes(int num);
